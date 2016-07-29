95df0371d2dca3cb9d32aff77e0c69fb240d65c0
break <dhpigtc@gmail.com>
2016-07-29 23:22:14 +0800

    完成注册验证,使用 hibernate 的 validation 实现

--- src/main/webapp/js/publicjs.js
+++ src/main/webapp/js/publicjs.js
@@ -22,3 +22,15 @@ function addphoto() {
     window.location.href = "../album/gotoAddPhoto";
     
 }
+
+function checkpwd() {
+
+    var pwd1 = document.getElementById("pwd1");
+    var pwd2 = document.getElementById("pwd2");
+
+
+    if(!(pwd1.value == pwd2.value)) {
+        document.getElementById("error").innerHTML = "<span class='error'>密码前后不一致,请重新输入</span>";
+      return false;
+    }
+}
